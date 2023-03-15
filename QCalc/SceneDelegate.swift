13866d6ea773e2d858ddb1add8dae35bdcf00094
Alexander Altman <qewhouse@gmail.com>
2023-03-16 00:28:34 +0300

    initial commit

--- QCalc/SceneDelegate.swift
+++ QCalc/SceneDelegate.swift
@@ -13,10 +13,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        // 1. Scene capture
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // 2. Creating of UIWindow with scene capture maker
+        let window = UIWindow(windowScene: windowScene)
+        //3. Making main ViewController with navigation bar
+        let viewCOntroller = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewCOntroller)
+        // 4. Making navigationController as rootViewController
+        window.rootViewController = navigationController
+        // 5. Setting the window back to Scene
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- QCalc/ViewController.swift
+++ QCalc/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemOrange
     }
 
 
+}
+
+import SwiftUI
+struct ListProvider: PreviewProvider {
+    static var previews: some View {
+        ContainterView().edgesIgnoringSafeArea(.all)
+            .previewDevice("iPhone 12 Pro Max")
+            .previewDisplayName("iPhone 12 Pro Max")
+        
+        ContainterView().edgesIgnoringSafeArea(.all)
+            .previewDevice("iPhone SE (3rd generation)")
+            .previewDisplayName("iPhone SE (3rd generation)")
+    }
+    
+    struct ContainterView: UIViewControllerRepresentable {
+        let listVC = ViewController()
+        func makeUIViewController(context:
+                                  UIViewControllerRepresentableContext<ListProvider.ContainterView>) -> ViewController {
+            return listVC
+        }
+        
+        func updateUIViewController(_ uiViewController:
+                                    ListProvider.ContainterView.UIViewControllerType, context:
+                                    UIViewControllerRepresentableContext<ListProvider.ContainterView>) {
+        }
+    }
 }
 
