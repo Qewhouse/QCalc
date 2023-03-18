b6200ce04222bbfd177858a98b4bedb0d43e52af
Alexander Altman <qewhouse@gmail.com>
2023-03-18 20:49:29 +0300

    background color changed

--- QCalc/Model/AppColor.swift
+++ QCalc/Model/AppColor.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 enum Appcolor {
-    static let background = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+    static let background = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
     static let topButtons = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
     static let actions = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
     static let buttons = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
